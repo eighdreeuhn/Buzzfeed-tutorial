3963b5f8219073655f79c854916aa30703c9ba26
Adrian Gioia <afgioia@gmail.com>
2022-12-03 22:54:25 -0800

    Buzzfeed tutorial

---
+++ src/Components/Answer.tsx
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const Answer = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export  default Answer
---
+++ src/Components/Questions.tsx
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const Questions = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export  default Questions
